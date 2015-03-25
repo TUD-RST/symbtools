930248c98e864d756a956a2edc936bf024cfd57c
Carsten Knoll <CarstenKnoll@gmx.de>
2015-03-25 15:12:03 +0100

    snapshot of wip: integration of some older code for handeling non_commutative matrices

---
+++ non_commutative_tools.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+
+import sympy as sp
+from sympy import Matrix, sin, cos
+from model_tools import generate_model, Rz
+from symb_tools import symbs_to_func, zip0
+import symb_tools as st
+
+import pickle
+
+from ipHelp import IPS, Tracer, ip_syshook, sys
+
+#ip_syshook(1)
+
+
+"""
+Collection of Code for noncommutative calculations ("s = d/dt")
+"""
+
+# time
+t = sp.Symbol('t', commutative = False)
+
+# Laplace-Variable (d/dt)
+s = sp.Symbol('s', commutative = False)
+
+
+def apply_deriv(term, power, s, t):
+    res = 0
+    assert int(power) == power
+    if power == 0:
+        return term
+
+
+    # !!stimmt das??
+    res = 0
+    for i in range(power):
+        tmp = term.diff(t) + term*s
+        res += apply_deriv(tmp, power-1, t, s).expand()
+
+    tmp = term.diff(t) + term*s
+    res = apply_deriv(tmp, power-1, s, t).expand()
+
+    return res
+
+
+def right_shift(mul, s, t, max_pow = 4):
+    """
+    Vorgehen:
+        index des ersten Auftauchens einer s-Potenz finden
+        -> mul = L *s**p * X* R0
+        -> s**p auf X anwenden -> es entstehen p + 1 Rest-Terme
+        -> Auf diese wird die Funktion rekursiv angewendet
+        -> Rückgabewert ist Mul oder Add von Muls wo die s-Terme ganz rechts stehen
+    """
+
+    assert isinstance(s, sp.Symbol)
+
+    # nothing to do
+    if s not in mul:
+        return mul
+
+    if not mul.expand().count_ops() == mul.count_ops():
+        raise ValueError, 'mul expression must be expanded:'+ str(mul)
+
+    if not isinstance(mul, sp.Mul):
+        if mul == s:
+            return mul
+        elif isinstance(mul, sp.Pow) and mul.args[0] == s:
+            return mul
+        else:
+            raise ValueError, 'Expected Mul, Symbol or Pow (like s**2), not ' +  str(Mul)
+    assert isinstance(mul, sp.Mul)
+    assert s.is_commutative == False
+
+    s_terms = [s**(i+1) for i in range(max_pow)]
+    args = mul.args
+
+    if not t in mul:
+        # aggregate all s-Terms at the right margin
+        # handle: s*a*s**2*b*s*c -> a*b*c*s**4
+        s_terms = []
+        rest = []
+        for a in args:
+            if s in a:
+                s_terms.append(a)
+            else: rest.append(a)
+        new_args = rest + s_terms
+        return sp.Mul(*new_args)
+
+    idx = min([args.index(st) for st in s_terms if st in args])
+
+    if not idx < len(args)-1:
+        # s already is at the right
+        return mul
+
+    L = sp.Mul(*args[:idx]) # left term
+    C = args[idx] # current term
+    N = args[idx+1] # next term
+    R0 = sp.Mul(*args[idx+2:]) # all right terms
+    if not t in N:
+        assert t in R0
+        return L*N*right_shift(C*R0, s, t)
+
+    exponent = C.as_base_exp()[1]
+    N_new = apply_deriv(N, exponent, s, t)
+    assert isinstance(N_new, sp.Add)
+
+    res = 0
+    for a in N_new.args:
+        if not a.count_ops() == a.expand().count_ops():
+            IPS()
+        tmp = L*right_shift(a*R0, s, t, max_pow)
+        res+= tmp.expand()
+
+    return res
+
+
+def right_shift_all(sum_exp, s, t, max_pow=4):
+    """
+    applies the right_shift to all arguments of a sum
+    if sum only consists of one arg this is also accepted
+    """
+
+    if isinstance(sum_exp, sp.Matrix):
+        def fnc(a):
+            return right_shift_all(a, s, t, max_pow)
+        return sum_exp.applyfunc(fnc)
+
+    assert isinstance(sum_exp, sp.Basic)
+
+    if isinstance(sum_exp, sp.Add):
+        args = sum_exp.args
+    elif isinstance(sum_exp, (sp.Mul, sp.Atom)):
+        args = (sum_exp,)
+    else:
+        raise ValueError, "unexpected type: %s" %type(sum_exp)
+
+    res = 0
+    for a in args:
+        assert isinstance(a, (sp.Mul, sp.Atom))
+        res += right_shift(a, s, t, max_pow)
+
+
+    return res
+
+def make_all_symbols_commutative(expr, appendix='_c'):
+    '''
+    :param expr:
+    :return: expr (with all symbols commutative) and
+              a subs_tuple_list [(s1_c, s1_nc), ... ]
+    '''
+
+    symbs = st.atoms(expr, sp.Symbol)
+
+    nc_symbols = [s for s in symbs if s.is_commutative == False]
+
+    new_symbols = [sp.Symbol(s.name+appendix, commutative=True)
+                   for s in symbs]
+
+    tup_list = zip(new_symbols, nc_symbols)
+    return expr.subs(zip(nc_symbols, new_symbols)), tup_list
+
+
+if __name__ == "__main__":
+    p = sp.Function("p")(t)
+    f = cos(p)
+
+    w = s**2*f
+
+    r = right_shift(w, s, t)
+
+
+    ()
---
+++ test_nc_tools.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri 2015-03-20
+
+@author: Carsten Knoll
+"""
+
+import unittest
+
+import sympy as sp
+from sympy import sin, cos, exp
+
+import symb_tools as st
+from IPython import embed as IPS
+import non_commutative_tools as nct
+
+# Noncommutative tools
+t = nct.t
+s = nct.s
+
+
+class NonCommToolsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_right_shift(self):
+        a, b = sp.symbols("a, b")
+        f1 = sp.Function('f1')(t)
+        f1d = f1.diff(t)
+        f2 = sp.Function('f2')(t)
+
+        res1 = nct.right_shift(s*f1, s, t)
+        ex1 = f1.diff(t) + f1*s
+
+        self.assertEquals(res1, ex1)
+
+        res2 = nct.right_shift(f2*s*f1, s, t)
+        ex2= f2*f1.diff(t) + f2*f1*s
+
+        self.assertEquals(res2, ex2)
+
+        res3 = nct.right_shift(a*f2*s*f1d, s, t)
+        ex3= a*f2*f1.diff(t, 2) + a*f2*f1d*s
+
+        self.assertEquals(res3, ex3)
+
+        res4 = nct.right_shift(s*f1*f2, s, t)
+        ex4 = f1.diff(t)*f2 + f1*f2*s + f1*f2.diff(t)
+
+        self.assertEquals(res4, ex4)
+
+        self.assertRaises( ValueError, nct.right_shift, s*f1*(f2+1), s, t )
+
+    def test_right_shift2(self):
+        a, b = sp.symbols("a, b", commutative = False)
+        f1 = sp.Function('f1')(t)
+        f1d = f1.diff(t)
+        f2 = sp.Function('f2')(t)
+
+        res1 = nct.right_shift(s*t, s, t)
+        ex1 = 1 + t*s
+
+        self.assertEquals(res1, ex1)
+
+        res2 = nct.right_shift(s, s, t)
+        ex2 = s
+        self.assertEquals(res2, ex2)
+
+        res3 = nct.right_shift(s**4, s, t)
+        ex3 = s**4
+        self.assertEquals(res3, ex3)
+
+        res4 = nct.right_shift(s**4*a*b, s, t)
+        ex4 = a*b*s**4
+        self.assertEquals(res4, ex4)
+
+        res5 = nct.right_shift(s**2*a*s*b*s, s, t)
+        ex5 = a*b*s**4
+        self.assertEquals(res5, ex5)
+
+        res6 = nct.right_shift(s**2*(a*t**3), s, t)
+        ex6 = a*(6*t + 6*t**2*s + t**3*s**2)
+        self.assertEquals(res6, ex6)
+
+    def test_right_shift3(self):
+        a, b = sp.symbols("a, b", commutative = False)
+        f1 = sp.Function('f1')(t)
+        f2 = sp.Function('y2')(t)
+        f1d = f1.diff(t)
+        f1dd = f1.diff(t, 2)
+        f2d = f2.diff(t)
+        f2dd = f2.diff(t, 2)
+
+        res1 = nct.right_shift(s*f1d*f2d, s, t)
+        ex1 = f1dd*f2d + f1d*f2dd + f1d*f2d*s
+
+        self.assertEquals(res1, ex1)
+
+        test = s*f2*f2d
+        # IPS()
+        print test
+        res2 = nct.right_shift(test, s, t)
+        ex2 = f2d**2 + f2*f2dd + f2*f2d*s
+
+        self.assertEquals(res2, ex2)
+
+    def _test_right_shift_all(self):
+        1/0
+        a, b = sp.symbols("a, b")
+        f1 = sp.Function('f1')(t)
+        f1d = f1.diff(t)
+        f2 = sp.Function('f2')(t)
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
