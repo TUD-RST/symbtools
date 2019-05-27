8e532d7e6e7af4fec3185d8a779d863dce247c75
Carsten Knoll <carsten.knoll@posteo.de>
2019-05-27 21:09:37 +0200

    improve mpctools.casadify and add optional tests

--- symbtools/mpc_tools.py
+++ symbtools/mpc_tools.py
@@ -1,16 +1,91 @@
 
 import casadi as cs
-from casadi import SX
+# noinspection PyUnresolvedReferences
+from casadi.casadi import SX
+import math
 
 import os
-from sympy.printing.lambdarepr import lambdarepr
+from sympy.printing.lambdarepr import lambdarepr, LambdaPrinter
+
+
+class CassadiPrinter(LambdaPrinter):
+    """
+    This subclass serves to convert scalar sympy expressions to casady functions.
+
+    It is strongly inspired by sympy.printing.lambdarepr.NumExprPrinter
+    """
+    _default_settings = {'fully_qualified_modules': False, 'inline': True,
+                         'allow_unknown_functions': True, 'order': None,
+                         'human': True,
+                         'full_prec': True,
+                         'user_functions': {}}
+
+    printmethod = "_numexprcode"
+
+    _numexpr_functions = {
+        'sin': 'sin',
+        'cos': 'cos',
+        'tan': 'tan',
+        'asin': 'arcsin',
+        'acos': 'arccos',
+        'atan': 'arctan',
+        'atan2': 'arctan2',
+        'sinh': 'sinh',
+        'cosh': 'cosh',
+        'tanh': 'tanh',
+        'asinh': 'arcsinh',
+        'acosh': 'arccosh',
+        'atanh': 'arctanh',
+        'log': 'log',
+        'exp': 'exp',
+        'sqrt': 'sqrt',
+        'Abs': 'fabs',
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # a few functions are called differently in sympy and casadi
+        # here we create the mapping {a: b, ...} where a is the name of the sympy func and b the name of cs func
+        self.cs_func_keys = dict([(key, value) for key, value in self._numexpr_functions.items()])
+
+        # some functions simply do not exisit in casadi
+        self.unsupported_funcs = ["conjugate", "im", "re", "where", "complex", "contains"]
+        for k in self.unsupported_funcs:
+            pass
+            # del self.cs_func_keys[k]
+
+        self.cs_funcs = dict([(value, getattr(cs, value)) for key, value in self.cs_func_keys.items()])
+
+        for k in self._numexpr_functions:
+            setattr(self, '_print_%s' % k, self._print_Function)
+
+    def _print_Function(self, e):
+        func_name = e.func.__name__
+
+        nstr = self._numexpr_functions.get(func_name, None)
+        if nstr is None:
+            raise TypeError("numexpr does not support function '%s'" %
+                                func_name)
+        return "%s(%s)" % (nstr, self._print_seq(e.args))
+
+    def _print_seq(self, seq, delimiter=', '):
+        s = [self._print(item) for item in seq]
+        if s:
+            return delimiter.join(s)
+        else:
+            return ""
 
 
 def casidify(expr, state_vect, input_vect):
     # source: https://gist.github.com/cklb/60362e1f49ef65f5212fb5eb5904b3fd
     """
-    Convert a sympy expression into a casadi expression
+    Convert a sympy-expression into a casadi expression
+
+    :param expr:        symbolic expression which is to convert to casadi
+    :param state_vect:  symbolic state vector
+    :param input_vect:  symbolic input vector
     """
+
     syms = []
     res = ["rhs = vertcat("]
     state_str = ["x = vertcat("]
@@ -29,9 +104,10 @@ def casidify(expr, state_vect, input_vect):
     input_str.append(")")
 
     # convert expression
+    CP = CassadiPrinter()
     for entry in expr:
         # handle expr
-        _expr = lambdarepr(entry)
+        _expr = CP.doprint(entry)
         res.append(_expr + ", ")
 
     res.append(")")
@@ -41,11 +117,20 @@ def casidify(expr, state_vect, input_vect):
                               + state_str
                               + input_str)
 
-    scope = dict(SX=cs.SX, MX=cs.MX, vertcat=cs.vertcat, sin=cs.sin, cos=cs.cos)
+    scope = dict(SX=cs.SX, MX=cs.MX, vertcat=cs.vertcat, **CP.cs_funcs)
     exec(ode_str, scope)
 
     return scope["rhs"], scope["x"], scope["u"]
 
+
+def create_casadi_func(sp_expr, sp_xx, sp_uu, name="rhs_cs"):
+    expr_cs, xx_cs, uu_cs = casidify(sp_expr, sp_xx, sp_uu)
+    func_cs = cs.Function('name', (xx_cs, uu_cs), (expr_cs,))
+
+    func_cs.xx = xx_cs
+    func_cs.uu = uu_cs
+
+    return func_cs
 
 # convenience functions (maybe there is a more elegant way)
 
@@ -68,6 +153,15 @@ def seq_to_SX_matrix(seq):
         for i, elt in enumerate(seq):
             res[:, i] = elt
         return res
+
+
+# noinspection PyPep8Naming
+def SX_diag_matrix(seq):
+    n = len(seq)
+    res = SX.zeros(n, n)
+    for i, elt in enumerate(seq):
+        res[i, i] = elt
+    return res
 
 
 def unpack(sx_matrix):
---
+++ symbtools/tests/test_mpctools.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created 2019-05-27 17:56:03
+
+@author: Carsten Knoll
+"""
+
+import unittesthelper as uth
+import unittest
+import sys
+import sympy as sp
+import symbtools as st
+import symbtools.mpc_tools as mpc
+import numpy as np
+
+
+from IPython import embed as IPS
+
+
+class MPCToolsTest(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(2001)
+
+    @uth.optional_dependency
+    def test_conversion1(self):
+        x1, x2, x3 = xx = st.symb_vector("x1:4")
+        u1, u2 = uu = st.symb_vector("u1:3")
+
+        expr_sp = sp.Matrix([x1 + x2 + x3, sp.sin(x1)*x2**x3, 1.23, 0, u1*sp.exp(u2)])
+        func_cs = mpc.create_casadi_func(expr_sp, xx, uu)
+
+        xxuu = list(xx) + list(uu)
+        func_np = st.expr_to_func(xxuu, expr_sp)
+        argvals = np.random.rand(len(xxuu))
+
+        argvals_cs = (argvals[:len(xx)], argvals[len(xx):])
+
+        res_np = func_np(*argvals)
+        res_cs = func_cs(*argvals_cs).full().squeeze()
+        self.assertTrue(np.allclose(res_np, res_cs))
+
+    @uth.optional_dependency
+    def test_conversion_all_funcs(self):
+        x1, x2, x3 = xx = st.symb_vector("x1:4")
+        u1, u2 = uu = st.symb_vector("u1:3")
+
+        xxuusum = sum(xx) + sum(uu)
+
+        arg = sp.tanh(xxuusum)  # limit the argument to (-1, 1)*0.99
+
+        # see mpc.CassadiPrinter.__init__ for exlanation
+        sp_func_names = mpc.CassadiPrinter().cs_func_keys.keys()
+
+        blacklist = ["atan2", ]
+        flist = [getattr(sp, name) for name in sp_func_names if name not in blacklist]
+
+        # create the test_matrix
+        expr_list = []
+        for func in flist:
+            if func is sp.acosh:
+                # only defined for values > 1
+                expr_list.append(func(1/arg))
+            else:
+                expr_list.append(func(arg))
+        expr_sp = sp.Matrix(expr_list + [arg, xxuusum])
+
+        func_cs = mpc.create_casadi_func(expr_sp, xx, uu)
+
+        xxuu = list(xx) + list(uu)
+        func_np = st.expr_to_func(xxuu, expr_sp)
+        argvals = np.random.rand(len(xxuu))
+
+        argvals_cs = (argvals[:len(xx)], argvals[len(xx):])
+
+        res_np = func_np(*argvals)
+        res_cs = func_cs(*argvals_cs).full().squeeze()
+        self.assertTrue(np.allclose(res_np, res_cs))
+
+
+def main():
+    # remove command line args which should not be passed to the testframework
+    if 'all' in sys.argv:
+        sys.argv.remove('all')
+
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
