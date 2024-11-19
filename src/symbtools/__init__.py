7c9c6b16d815f461ab7772015851123ba07cd799
Carsten Knoll <carsten.knoll@posteo.de>
2024-11-19 14:34:41 +0100

    switch from setup.py to pyproject.toml

--- src/symbtools/__init__.py
+++ src/symbtools/__init__.py
@@ -1,10 +1,23 @@
-# -*- coding: utf-8 -*-
+from .release import __version__
+
+# the package is imported during installation
+# however installation happens in an isolated build environment
+# where no dependencies are installed.
+
+# this means: no importing the following modules will fail
+# during installation. This is OK (but only during installation)
+
 
 try:
     from .core import *
 except ImportError:
+    import os
+    if "PIP_BUILD_TRACKER" in os.environ:
+        pass
+    else:
+        # raise the original exception
+        raise
+
     # this might be relevant during the installation process
     # otherwise setup.py cannot be executed
     pass
-
-from .release import __version__
